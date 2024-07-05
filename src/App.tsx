b9560d086bc3bd7d2c5f2a57dbb37764570fa904
jauafelipe <jauafelipe345@gmail.com>
2024-07-05 20:22:54 -0300

    correçao de rota abou para start

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css'
-import { ProgressBar } from './components/motion-progress-bar/Progress-bar'
 import { StartHome } from './pages/StartHome/StartHome'
 
 function App() {
 
   return (
     <>
-
-<ProgressBar />
-
       <StartHome />
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path='/'  element={<App />} loader />
-      <Route path='/about-me' element={<StartPainel />}/>
+      <Route path='/start' element={<StartPainel />}/>
       <Route path='home' element={<HomePage/>} />
     </Routes>
   </BrowserRouter>
